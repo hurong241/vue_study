e72d382980956cfa679c1747771e1f83fe8bd2a0
胡熔 <3136875651@qq.com>
2024-01-03 18:50:21 +0800

    自定义事件实现组件通讯

--- vue2-cli/src/App80.vue
+++ vue2-cli/src/App80.vue
@@ -2,6 +2,9 @@
   <div class="app">
     <div>
       p80-83:组件间通讯方法2-自定义事件实现：<br/>
+      通过组件上传参，并自定义事件方法名传给子组件，<br/>
+      子组件通过$emit触发自定义事件,$off销毁自定义事件实现通讯<br/>
+      如果要用原生事件要加上修饰符native,如：@click.native，否则它就是一个算义的click事件<br/>
       <hr/>
     </div>
     <School></School>
